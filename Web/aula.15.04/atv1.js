3f7f7273d6204dba6843894066989e4698827dee
d8tsme <datgrjd4vdzz@gmail.com>
2025-04-15 16:59:21 -0300

    aula1504

---
+++ Web/aula.15.04/atv1.js
@@ -0,0 +1,13 @@
+// Funcao soma da pa
+
+function somaPA(a1, an, n){
+    return ((a1+an) * n)/2;
+}
+
+console.log(somaPG(10, 2, 5))
+
+// funcao soma pg
+
+function somaPG(a1, q, n){
+    return (a1*(q**n-1))/(q-1)
+}
